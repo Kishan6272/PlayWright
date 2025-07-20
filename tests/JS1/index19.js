01a92ae4afce7475403b212aa8722962e77b36ca
Kishan6272 <kishanpandey6272@gmail.com>
2025-07-20 22:57:02 +0530

    html modifies

--- tests/JS1/index19.js
+++ tests/JS1/index19.js
@@ -1,10 +1,33 @@
-let name1 ="harry";
+let name1 =[
+    ["harry",
+        "harry",
+        "harry",
+        "harry",
+        "harry",
+
+    ]
+];
+
 
 let arr=Array.from(name1);
+
+let arr1=
 
 console.log(arr);
 console.log(typeof arr);
 
 console.log(typeof name1);
 // for of
+
+for(let item of name1)
+    {
+        console.log(item)
+    }
+
+    for(let i of name1[0])
+    {
+        console.log(name1[i]);
+    }
+
+
 
